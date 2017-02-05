576c01c9f9fc751521f8f8b0cda037b53345478a
kkiple93 <kkiple93@gmail.com>
2017-02-05 14:52:19 -0600

    Init

---
+++ guildController.py
@@ -0,0 +1,8 @@
+__author__ = 'Kyle'
+from WoW import RaiderStats, MythicsRan
+
+playerList = []
+playerObject = {}
+MythicsRan.main(playerList, playerObject)
+RaiderStats.main(playerList,playerObject)
+
